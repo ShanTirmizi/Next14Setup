0ceebeefd2ec8588191e777ae7c18916d9e694de
Shan Tirmizi <61161336+ShanTirmizi@users.noreply.github.com>
2024-02-26 08:59:57 +0000

    added migration to test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from '@/components/ui/button';
+import { prisma } from '@/lib/prisma';
 
-export default function Home() {
+export default async function Home() {
+  const user = await prisma.test.findFirst();
+  if (!user) return <p>No user found</p>;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>{user?.name}</p>
       <Button className="m-auto">Click me</Button>
     </main>
   );
